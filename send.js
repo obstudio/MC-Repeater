eea195a4a79f5ec236a6df891865bc23b9cf8d77
NN708 <yuqian_1998@126.com>
2019-11-23 19:09:00 +0800

    Modify code for API

--- send.js
+++ send.js
@@ -1,15 +1,21 @@
 const https = require('https')
+const crypto = require('crypto')
 const config = require('./config.json')
 
 function send(info) {
-  const options = {
-    hostname: config.botServerHost,
-    path: `/subscribe?channel=ob-server&type=${info['type']}&message=${info['message']}`,
+  const msg = info['message']
+  if (msg) {
+    const msgShort = msg.length > 20 ? msg.slice(0, 10) + msg.length + msg.slice(-10) : msg
+    const sign = crypto.createHash('md5').update(config.id + msgShort + salt + config.key).digest('hex')
+    const options = {
+      hostname: config.botServerHost,
+      path: `/subscribe/:${config.id}?salt=${salt}&sign=${sign}&msg=${msg}`,
+    }
+    https.get(options, (res) => {
+      const { statusCode } = res
+      if (statusCode !== 200) console.log('Request sending failed')
+    })
   }
-  https.get(options, (res) => {
-    const { statusCode } = res
-
-  })
 }
 
 module.exports = send
