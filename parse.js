deb0c317c2e54cd76af5c318b577929b2d0bd4e7
NN708 <yuqian_1998@126.com>
2019-11-24 16:01:43 +0800

    Add i18n & user message forwarding

--- parse.js
+++ parse.js
@@ -1,4 +1,7 @@
 const config = require('./config.json')
+const i18n = require('./i18n.json')
+
+const lang = i18n[config['language']]
 
 function parse(text) {
   switch (config.serverType) {
@@ -8,35 +11,36 @@ function parse(text) {
 }
 
 function parseJava(text) {
-  const reg = /\[\d{2}:\d{2}:\d{2}\] \[Server thread\/INFO\]: (.*)/
+  const reg = /^\[\d{2}:\d{2}:\d{2}\] \[Server thread\/INFO\]: (.*)$/
   res = reg.exec(text)
   if (!res) return
 
   text = res[1]
-  const regJoin = /\w+ joined the game/
-  const regLeave = /\w+ left the game/
-  const regStart = /Done \([0-9\.]+s\)! For help, type "help"/
-  const regStop = /Stopping the server/
+  const regJoin = /^(\w+) joined the game$/
+  const regLeave = /^(\w+) left the game$/
+  const regStart = /^Done \([0-9\.]+s\)! For help, type "help"$/
+  const regStop = /^Stopping the server$/
+  const regMsg = /^<(\w+)> (.+)$/
   info = undefined
-  if (regJoin.exec(text)) {
+  if (res = regJoin.exec(text)) {
     info = {
-      'type': 'join',
-      'message': text
+      'message': res[1] + ' ' + lang['join']
     }
-  } else if (regLeave.exec(text)) {
+  } else if (res = regLeave.exec(text)) {
     info = {
-      'type': 'leave',
-      'message': text
+      'message': res[1] + ' ' + lang['leave']
     }
   } else if (regStart.exec(text)) {
     info = {
-      'type': 'start',
-      'message': 'Server started'
+      'message': lang['start']
     }
   } else if (regStop.exec(text)) {
     info = {
-      'type': 'stop',
-      'message': 'Server stopped'
+      'message': lang['stop']
+    }
+  } else if (res = regMsg.exec(text)) {
+    info = {
+      'message': res[1] + ': ' + res[2]
     }
   }
   return info
