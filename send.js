6904585e8ac0dd83385139863c6e6c6df0dec8de
NN708 <yuqian_1998@126.com>
2019-11-25 18:44:51 +0800

    Remove test code

--- send.js
+++ send.js
@@ -5,19 +5,18 @@ const config = require('./config')
 function send(info) {
   const msg = info.message
   if (msg) {
-    // const msgShort = msg.length > 20 ? msg.slice(0, 10) + msg.length + msg.slice(-10) : msg
-    // const salt = crypto.randomBytes(4).toString('hex')
-    // const sign = crypto.createHash('md5').update(config.channelId + msgShort + salt + config.key).digest('hex')
-    // const path = `/subscribe/${config.channelId}?salt=${salt}&sign=${sign}&msg=${encodeURIComponent(msg)}`
-    // const options = {
-    //   hostname: config.botServerHost,
-    //   path: path,
-    // }
-    // https.get(options, (res) => {
-    //   const { statusCode } = res
-    //   if (statusCode !== 200) console.log('Request sending failed')
-    // })
-    console.log(msg)
+    const msgShort = msg.length > 20 ? msg.slice(0, 10) + msg.length + msg.slice(-10) : msg
+    const salt = crypto.randomBytes(4).toString('hex')
+    const sign = crypto.createHash('md5').update(config.channelId + msgShort + salt + config.key).digest('hex')
+    const path = `/subscribe/${config.channelId}?salt=${salt}&sign=${sign}&msg=${encodeURIComponent(msg)}`
+    const options = {
+      hostname: config.botServerHost,
+      path: path,
+    }
+    https.get(options, (res) => {
+      const { statusCode } = res
+      if (statusCode !== 200) console.log('Request sending failed')
+    })
   }
 }
 
