350af619ec32e630e0e3d9781022f25e971de28b
NN708 <yuqian_1998@126.com>
2019-11-25 18:58:36 +0800

    Fix regMsg

--- parse.js
+++ parse.js
@@ -310,7 +310,7 @@ function parseJava(text) {
   const regLeave = new RegExp(`^(${vchar}+) left the game$`)
   const regStart = /^Done \([0-9\.]+s\)! For help, type "help"$/
   const regStop = /^Stopping the server$/
-  const regMsg = new RegExp(`^<(${vchar}+)>: (.+)$`)
+  const regMsg = new RegExp(`^<(${vchar}+)> (.+)$`)
   const regServerMsg = new RegExp(`^\\[(${vchar}+)\\] (.+)$`)
   info = undefined
   if (res = regJoin.exec(text)) {
